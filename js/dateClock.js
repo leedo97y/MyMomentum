9728c0c271dfea9cba0496b5880ee78f5db8b791
leedo97y <dlehdus1433@gmail.com>
2022-07-16 21:36:38 +0900

    시계 부분 바꾸기

--- js/dateClock.js
+++ js/dateClock.js
@@ -22,8 +22,8 @@ function date() {
   const dayText = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   const year = today.getFullYear();
-  const month = monthText[today.getMonth()];
-  const date = today.getDate();
+  const month = monthText[today.getMonth()].padStart(2, "0");
+  const date = JSON.stringify(today.getDate()).padStart(2, "0");
   const day = dayText[today.getDay()];
 
   datePart.innerText = `${year} ${month}/${date} ${day}`;
@@ -33,9 +33,9 @@ function date() {
 function clock() {
   const today = new Date();
 
-  const hours = today.getHours();
-  const minutes = today.getMinutes();
-  const seconds = today.getSeconds();
+  const hours = JSON.stringify(today.getHours()).padStart(2, "0");
+  const minutes = JSON.stringify(today.getMinutes()).padStart(2, "0");
+  const seconds = JSON.stringify(today.getSeconds()).padStart(2, "0");
 
   clockPart.innerText = `${hours}:${minutes}:${seconds}`;
 }
