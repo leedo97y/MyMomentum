77b6b102f7b54726a0813c0ee3407f6631f5f706
leedo97y <dlehdus1433@gmail.com>
2022-07-16 21:04:50 +0900

    날씨 기능 추가, 아이콘 추가

---
+++ js/weather.js
@@ -0,0 +1,26 @@
+const API_KEY = "c085c85ef7adac74dca0cca6afe4cb80";
+
+const weather = document.querySelector("#weather");
+const icon = document.querySelector("#icon");
+const city = document.querySelector("#city");
+
+function getGeo(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = `${data.name}`;
+      icon.innerHTML = `<img src=${iconUrl} />`;
+    });
+}
+
+function getGeoError() {
+  alert("error!");
+}
+
+navigator.geolocation.getCurrentPosition(getGeo, getGeoError);
