507c0a7b8387b66efa8f197aa628f623c314d096
leedo97y <dlehdus1433@gmail.com>
2022-07-16 22:19:55 +0900

    배경 JS 파일에서 색 조정, css text-align 추가

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@ const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 
 const randomBg = images[Math.floor(Math.random() * images.length)];
 
-const background = document.createElement("img");
-background.src = `img/${randomBg}`;
-
-body.appendChild(background);
+body.style.backgroundImage = `linear-gradient(
+  rgba(0, 0, 0, 0.3),
+  rgba(0, 0, 0, 0.3)
+),url("img/${randomBg}")`;
