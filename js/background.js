cdb15dbdf6a8b62926bfbad75cb93105ff05edc6
leedo97y <dlehdus1433@gmail.com>
2022-07-16 21:30:41 +0900

    배경 추가

--- js/background.js
+++ js/background.js
@@ -1,1 +1,9 @@
+const body = document.body;
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+
+const randomBg = images[Math.floor(Math.random() * images.length)];
+
+const background = document.createElement("img");
+background.src = `img/${randomBg}`;
+
+body.appendChild(background);
