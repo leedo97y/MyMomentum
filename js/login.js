c4f71158fccc4c180dbf020f92502e6101136fe7
leedo97y <dlehdus1433@gmail.com>
2022-07-18 01:41:50 +0900

    로그인 타이틀 수정

--- js/login.js
+++ js/login.js
@@ -4,8 +4,6 @@ const title = document.querySelector("title");
 
 const greetings = document.querySelector("#greetings");
 
-title.innerText = "My page";
-
 const USERID = "userId";
 
 function onLoginSubmit(event) {
@@ -29,18 +27,18 @@ function greetingText() {
 const savedUserId = localStorage.getItem(USERID);
 
 if (savedUserId === null) {
+  // 페이지 이름 바꾸기
+  title.innerText = "My page";
   // 저장된 userId가 없다면 로그인 form을 보이지 않게 만들어 준다.
   loginForm.classList.remove("hidden");
   // 그리고 입력한 아이디를 submit 해준다.
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
+  title.innerText = `${savedUserId}'s page`;
   // 이 부분은 저장된 아이디가 있을떄 h1 부분에 innerText를 지정해주는 것이다.
   greetingText();
 }
 // 이렇게 하고 새로고침하면... 다시 form이 생성되기 때문에 애초에 login-form에 class="hidden"을 부여한다.
-
-title.innerText = `${savedUserId}'s page`;
-// 페이지 이름 바꾸기
 
 function loginFocus() {
   loginInput.placeholder = "";
