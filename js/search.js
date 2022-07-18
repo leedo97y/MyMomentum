bbc4d39d2b153ee131abed8ee7366081ad07edb2
leedo97y <dlehdus1433@gmail.com>
2022-07-19 01:19:48 +0900

    검색 기능 디자인 완료, JS 기능 구현 진행중

---
+++ js/search.js
@@ -0,0 +1,11 @@
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-form input");
+const searchButton = document.querySelector("#search-form button");
+
+function handleSearchButton(event) {
+  event.preventDefault();
+  // 1. get input value
+  // 2. search for it
+}
+
+searchButton.addEventListener("submit", handleSearchButton);
