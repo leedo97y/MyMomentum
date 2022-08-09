c8c1c1e728a12cc99ebac81e8e14bcdb0802d403
leedo97y <dlehdus1433@gmail.com>
2022-08-09 22:23:31 +0900

    시간에 따라 베경이 바뀌게 만들기

--- js/background.js
+++ js/background.js
@@ -1,18 +1,29 @@
 const body = document.body;
-const images = [
-  "0.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-];
 
-const randomBg = images[Math.floor(Math.random() * images.length)];
+const today = new Date();
+const hours = today.getHours();
 
-body.style.backgroundImage = `linear-gradient(
-  rgba(0, 0, 0, 0.3),
-  rgba(0, 0, 0, 0.3)
-),url("img/${randomBg}")`;
+const sunnyImages = ["sunny1.png", "sunny2.jpg", "sunny3.jpg", "sunny4.png"];
+const sunsetImages = ["sunset1.jpg", "sunset2.jpg", "sunset3.jpg"];
+const nightImages = ["night1.png", "night2.jpg"];
+
+if (hours < 5 || hours >= 19) {
+  const randomBg3 = nightImages[Math.floor(Math.random() * nightImages.length)];
+  body.style.backgroundImage = `linear-gradient(
+      rgba(0, 0, 0, 0.15),
+      rgba(0, 0, 0, 0.15)
+    ),url("img/${randomBg3}")`;
+} else if (hours < 19 && hours >= 15) {
+  const randomBg2 =
+    sunsetImages[Math.floor(Math.random() * sunsetImages.length)];
+  body.style.backgroundImage = `linear-gradient(
+      rgba(0, 0, 0, 0.15),
+      rgba(0, 0, 0, 0.15)
+    ),url("img/${randomBg2}")`;
+} else if (hours < 15 && hours >= 5) {
+  const randomBg1 = sunnyImages[Math.floor(Math.random() * sunnyImages.length)];
+  body.style.backgroundImage = `linear-gradient(
+      rgba(0, 0, 0, 0.15),
+      rgba(0, 0, 0, 0.15)
+    ),url("img/${randomBg1}")`;
+}
